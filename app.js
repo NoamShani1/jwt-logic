db18c16889f703e489dac244f664db6584b317ee
Noam Shani <Noams156@gmail.com>
2022-01-10 17:45:56 +0100

    .git deleted in client

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+const { isAuthenticated } = require('./middleware/jwt.js')
+
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
