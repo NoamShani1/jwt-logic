b58bc97ee4e973ef7e2f91980aae44e6abbf864f
Noam Shani <Noams156@gmail.com>
2022-01-21 17:41:01 +0100

    navbar added, login and signup added to front-end

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import Navbar from './components/Navbar';
+
 
 // import Login from './pages/Login'
 
@@ -10,8 +12,8 @@ function App() {
     <div className="App">
      
       <h1>Instagram</h1>
+      <Navbar />
       <Routes>
-       
         {/* <Route path='/login' element={<Login />} /> */}
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../context/auth'
+
+
+export default function Navbar() {
+
+    const { isLoggedIn, user, logoutUser } = useContext(AuthContext)
+
+    return (
+		<nav>
+
+			{isLoggedIn ?
+				(
+					<>
+						<Link to='/post/new'>
+							<button>create a tweet</button>
+						</Link>
+						<button onClick={logoutUser}>Logout</button>
+
+						<Link to='/profile'>
+							<button>Profile</button>
+						</Link>
+
+						<Link to='/'>
+							<button>Feed</button>
+						</Link>
+					</>
+				) : (
+					<>
+						<Link to='/signup'>
+							<button>Signup</button>
+						</Link>
+						<Link to='/login'>
+							<button>Login</button>
+						</Link>
+
+
+
+					</>
+				)}
+		</nav>
+	)
+}
